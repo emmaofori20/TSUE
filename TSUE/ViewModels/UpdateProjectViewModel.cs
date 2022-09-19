0b9013188798660ee311fa5c667e748d2010d6ab
Emmanuel Ofori <emmanuelofori2638@gmail.com>
2022-09-19 11:37:48 +0000

    Accepted changes from reset

---
+++ TSUE/ViewModels/UpdateProjectViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace TSUE.ViewModels
+{
+    public class UpdateProjectViewModel
+    {
+         public int ProjectId { get; set; }
+        public string ProjectOverview { get; set; }
+        [Required]
+        public string StudyTitle { get; set; }
+        public DateTime? YearOfPublication { get; set; }
+        public IFormFile ProjectIcon { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? UpdatedOn { get; set; }
+        public string UpdatedBy { get; set; }
+        public bool? IsDeleted { get; set; }
+        [Required]
+        public string Authors { get; set; }
+ 
+        public int ProjectCategoryId { get; set; }
+        [Required]
+        public int DocumentTypeId { get; set; }
+        public SelectList SelectDocumentType { get; set; }
+        [Required]
+        public int CountryId { get; set; }
+        public SelectList SelectCountry { get; set; }
+        public int LanguageId { get; set; }
+        public SelectList SelectLanguage { get; set; }
+   
+        public byte[] ProjectIconByte { get; set; }
+
+        public List<ProjectDocumentForUpdate> ProjectDocumentsForUpdate { get; set; }
+
+    }
+
+    public class ProjectDocumentForUpdate
+    {
+        public int ProjectDocumentId { get; set; }
+        public int ProjectId { get; set; }
+        public string ProjectDocumentName { get; set; }
+        public byte[] ProjectDocumentByte { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime? UpdatedOn { get; set; }
+        public IFormFile ProjectDocumentFile { get; set; }
+
+    }
+}
